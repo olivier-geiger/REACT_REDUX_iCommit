5689770bc1fc92fb2fbd8da04f457084e376e9f1
oliver <olivier.codes@gmail.com>
2021-09-11 19:38:30 +0200

    add navbar

---
+++ src/components/layout/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <nav style={{ marginBottom: '30px' }} className='blue'>
+      <div className='nav-wrapper'>
+        <form>
+          <div className='input-field'>
+            <input id='search' type='search' required />
+            <label className='label-icon' forhtml='search'>
+              <i className='material-icons'>search</i>
+            </label>
+            <i className='material-icons'>close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+export default SearchBar;
